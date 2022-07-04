34379d6ad03d666201bdba5766d77f79bc2bf0d6
charlestietjen <tietjen.charles@gmail.com>
2022-07-04 14:05:21 -0400

    Repos endpoint - imported axios, wrote GET, imported repos.json, filtered results and return final data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,28 @@
 import { Router, Request, Response } from 'express';
+// Used axios to save some time
+import axios from 'axios'
+import internal from 'stream';
 
 export const repos = Router();
+const externalUrl = `https://api.github.com/users/silverorange/repos`
+
+// Asynchronously GET from silverorange repos
+const externalRequest = async () => {
+    try {
+      const resp = await axios.get(externalUrl);
+      return resp.data
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+// Provide an array, filter by the fork boolean to remove forked repos, spread the returned array
+const filterArray = (array: any[]) => {
+  const filteredArray = array.filter((arr: { fork: boolean; }) => {
+    return arr.fork === false
+  })
+  return [...filteredArray]
+}
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -8,5 +30,16 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  
+  // import the repos on a GET, I *think* this will ensure we pull current data rather than the JSON at server runtime, further testing if I have time remaining!
+  const internalRepos = require(`../../data/repos.json`);
+
+  // declare our external repos as the returned axios request
+  const externalRepos = await externalRequest();
+
+  // filter both arrays and apply spread operators
+  const filteredArrays = [...filterArray(internalRepos), ...filterArray(externalRepos)];
+
+  // return filtered arrays as JSON...apply spread...again.
+  res.json([...filteredArrays]);
 });
